b9ab5de18e44597a758e6380235a0fea14220c32
Cédric JUNG <cedced19@gmail.com>
2018-04-28 19:43:20 +0200

    Add conditions of use alert

--- app/screens/main.js
+++ app/screens/main.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react';
 import {
   StatusBar,
   StyleSheet,
-  Linking
+  Linking,
+  Alert
 } from 'react-native';
 
-import { Container, Title, Content, Button, Text, Icon, Grid, Row, Footer } from 'native-base';
+import { Container, Title, Content, Button, Text, Icon, Grid, Row, Footer, CheckBox, Body } from 'native-base';
 
 export default class MainScreen extends React.Component {
   static navigationOptions = {
@@ -15,6 +16,13 @@ export default class MainScreen extends React.Component {
     }
   };
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      accepted: false
+    }
+    this.acceptRules = this.acceptRules.bind(this);
+  }
 
   openMap() {
     Linking.canOpenURL('http://maps.google.com/maps?daddr=48.743448,7.258147').then(supported => {
@@ -24,8 +32,29 @@ export default class MainScreen extends React.Component {
     });
   };
 
+  acceptRules() {
+    if (this.state.accepted) {
+      this.props.navigation.navigate('Walking');
+    } else {
+      Alert.alert(
+        'Conditions d\'utilisation',
+        'En appuyant sur ce bouton vous acceptez les conditions d\'utilisation évoquées sur cette page.',
+        [
+          { text: 'Annuler', style: 'cancel' },
+          {
+            text: 'Oui',
+            onPress: () => {
+              this.props.navigation.navigate('Walking');
+              this.setState({ accepted: true })
+            }
+          },
+        ],
+        { cancelable: true }
+      )
+    }
+  }
+
   render() {
-    const { navigate } = this.props.navigation;
     return (
       <Container style={styles.container}>
         <StatusBar backgroundColor={'#335fe1'} />
@@ -46,7 +75,7 @@ export default class MainScreen extends React.Component {
               </Button>
             </Row>
             <Row style={styles.row}>
-              <Button onPress={() => navigate('Tutorial')} style={{ backgroundColor: '#335fe1' }} iconRight>
+              <Button onPress={() => this.props.navigation.navigate('Tutorial')} style={{ backgroundColor: '#335fe1' }} iconRight>
                 <Text>Tutoriel</Text>
                 <Icon name='ios-book-outline' />
               </Button>
@@ -54,10 +83,11 @@ export default class MainScreen extends React.Component {
           </Grid>
           <Text style={styles.instructions}>La distance du parcours est de <Text style={{ fontWeight: 'bold' }}>5 km</Text>.{'\n'}</Text>
           <Text style={styles.instructions}>La marche est considérée comme un sport par conséquent assurez-vous d'avoir les conditions physiques nécessaires pour pouvoir la pratiquer.{'\n'}</Text>
-          <Text style={styles.instructions}>Nous vous rappelons que cette application pour smartphone peut à tout moment être victime d'une panne ou d'une déficience technique. Vous ne devez par conséquent pas avoir une foi aveugle en elle et nous vous conseillons de toujours vous munir d'une carte lorsque vous allez en forêt.{'\n'}</Text>
+          <Text style={styles.instructions}>L'association Découverto, ses auteurs et ses collaborateurs déclinent toutes responsabilités quant à l'utilisation, l'exactitude et la manipulation de l'application.{'\n'}</Text>
+          <Text style={styles.instructions}>Nous vous rappelons que cette application pour smartphone peut à tout moment être victime d'une panne ou d'une déficience technique. Vous ne devez par conséquent pas avoir une foi aveugle en elle et nous vous conseillons de toujours vous munir d'une carte lorsque vous allez en forêt.</Text>
         </Content>
         <Footer style={{ backgroundColor: '#F5FCFF' }}>
-          <Button onPress={() => navigate('Walking')} style={{ backgroundColor: '#e74c3c' }} iconRight>
+          <Button onPress={this.acceptRules} style={{ backgroundColor: '#e74c3c' }} iconRight>
             <Text>Démarrer</Text>
             <Icon name='ios-arrow-forward-outline' />
           </Button>
